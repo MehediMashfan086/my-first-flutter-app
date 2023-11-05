529971ce03b14b08e95b2602c8c7a7a326586ece
Mehedi Hasan <mehedihasan62086@gmail.com>
2023-11-05 14:36:02 +0600

    feat(login): creating login page with username and password

--- first_flutter_project/lib/main.dart
+++ first_flutter_project/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:first_flutter_project/pages/home_page.dart';
 import 'package:first_flutter_project/pages/login_page.dart';
 import "package:flutter/material.dart";
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,8 +11,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
+          fontFamily: GoogleFonts.lato().fontFamily),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
       // home: HomePage(),
-      initialRoute: "/home",
+      initialRoute: "/",
       routes: {
         "/": (context) => LoginPage(),
         "/home": (context) => HomePage(),
